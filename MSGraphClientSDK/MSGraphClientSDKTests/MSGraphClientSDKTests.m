58e880ab3a198b725e976970f0c74e35a39b770f
Vikas Dadheech <vidadhee@microsoft.com>
2019-02-06 12:10:04 +0530

    Rename project to MSGraphClientSDK from MSGraphSDK

---
+++ MSGraphClientSDK/MSGraphClientSDKTests/MSGraphClientSDKTests.m
@@ -0,0 +1,37 @@
+//
+// Copyright (c) Microsoft Corporation. All Rights Reserved. Licensed under the MIT License. See License in the project root for license information.
+//
+
+#import "MSGraphClientSDKTests.h"
+
+@implementation MSGraphClientSDKTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    self.testBaseURL = [NSURL URLWithString:@"https://foo.com/bar/baz"];
+    self.requestForMock = [[NSMutableURLRequest alloc] initWithURL:self.testBaseURL];
+    self.mockAuthProvider = OCMProtocolMock(@protocol(MSAuthenticationProvider));
+    self.mockHttpProvider = OCMProtocolMock(@protocol(MSHttpProvider));
+    self.mockClient = OCMPartialMock([MSClientFactory createHTTPClientWithAuthenticationProvider:self.mockAuthProvider]);
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.mockAuthProvider = nil;
+    self.mockClient = nil;
+    [super tearDown];
+}
+
+-(void)completionBlockCodeInvoked{
+    _bCompletionBlockInvoked = YES;
+}
+-(void)checkCompletionBlockCodeInvoked{
+    XCTAssertTrue(_bCompletionBlockInvoked);
+}
+
+
+
+
+@end
