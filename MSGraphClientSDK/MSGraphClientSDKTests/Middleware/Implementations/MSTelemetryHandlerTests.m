99a84c006f88aa9ab949d8891e1b70957bc1ccbe
Vikas Dadheech <dadheech115@gmail.com>
2019-05-20 18:00:05 +0530

    Add telemetry handler and relevant changes

---
+++ MSGraphClientSDK/MSGraphClientSDKTests/Middleware/Implementations/MSTelemetryHandlerTests.m
@@ -0,0 +1,37 @@
+//
+//  MSTelemetryHandlerTests.m
+//  MSGraphClientSDKTests
+//
+//  Created by Vikas Dadheech on 20/05/19.
+//  Copyright © 2019 Vikas Dadheech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MSTelemetryHandlerTests : MSGraphClientSDKTests
+
+@end
+
+@implementation MSTelemetryHandlerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
